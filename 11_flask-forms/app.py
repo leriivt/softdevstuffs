e43ad91a3c398ae88a083331d4f212bcabfc2d65
leriivt <90656798+leriivt@users.noreply.github.com>
2022-10-17 00:06:05 -0400

    more notes and tests

--- 11_flask-forms/app.py
+++ 11_flask-forms/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+'''
+VSM: Verit, Sebastian, Maya
+SoftDev
+K11 -- forms in Flask
+2022-10-16
+time spent: 1.3
+'''
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -28,23 +32,27 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST']) #methods might make it so the app can only GET and POST
+@app.route("/", methods=['GET', 'POST']) #methods might make it so the app can only GET and POST
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app) #print __main__ in the terminal
+    print(app) #predict: print __main__ in the terminal ; actually prints <Flask 'app'> in the terminal (__main__ is from printing __name__)
+    print('------')
     print("***DIAG: request obj ***")
-    print(request) #will work, print what is requested by the form?
+    print(request) #predit: will work, print what is requested by the form? ; actually prints url requested by the user
     print("***DIAG: request.args ***")
-    print(request.args) 
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print(request.args) #prints the ImmutableMultiDict which seems like a way to collect user inputs
+    print("***DIAG: request.args['username']  ***")
+    try:
+        print(request.args['username']) #prints the the value associated with the key 'username' in ImmutableMultiDict
+    except:
+        print("ERROR: there is no username")  
     print("***DIAG: request.headers ***")
-    print(request.headers)
+    print(request.headers) #prints a variety of information including host ip address, user web browser, and user computer information
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth" , methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -53,8 +61,11 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***")
+    try:
+        print(request.args['username']) 
+    except:
+        print("ERROR: there is no username")
     print("***DIAG: request.headers ***")
     print(request.headers)
     return "Waaaa hooo HAAAH"  #response to a form submission
@@ -65,3 +76,4 @@ if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+
