99cc3162dde940144a3503fb54b591923599cadf
leriivt <90656798+leriivt@users.noreply.github.com>
2022-11-23 03:31:02 -0500

    heading

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,5 +1,10 @@
-
-
+'''
+stevejobsghost: Verit Li (Nibbles), Erica Li (Hugo)
+K20 - REST API
+Soft Dev
+2022-11-22
+time spent: 0.5 hr
+'''
 
 import requests
 from flask import Flask, render_template
@@ -12,14 +17,12 @@ with open('key_nasa.txt') as f:
 
 #print(key)
 nasa_url = "https://api.nasa.gov/planetary/apod?api_key=" + key
-print(nasa_url)
-res = requests.get(nasa_url)
-json = res.json()
-pic_url = json["url"]
+print(nasa_url) #prints the nasa_url with key to the console
+res = requests.get(nasa_url) 
+pic_url = json["url"] #finds the key named url in the json file 
 
 @app.route("/")
 def picture():
-    
     return render_template('main.html', url=pic_url)
     
 if __name__ == "__main__":
