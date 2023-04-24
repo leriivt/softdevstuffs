915bdf7c98d491fc6721311fb7ffdaa1ca157370
leriivt <verit.li@gmail.com>
2023-04-24 13:14:30 -0400

    init

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,48 @@
+//retrieve node in DOM via ID
+var c = Document.getContext("Slate")
+
+//instantiate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+//var toggleMode = function(e) {
+var toggleMode = (e) => {
+    console.log("toggling...");
+    if () {
+
+
+    }
+    else {
+
+    }
+}
+
+var drawRect = function(e) {
+    var event = MouseEvent();
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+    console.log("mouseclick registered at ", mouseX, mouseY);
+
+    fillRect(mouseX, mouseY, 100, 200);
+}
+
+//var drawCircle = function(e) {
+var drawCircle = (e) => {
+    console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var draw = function() {
+var draw = (e) => {
+
+}
+
+//var wipeCanvas = function() {
+var wipeCanvas = () => {
+
+}
+
+c.addEventListener("click", draw);
+var bToggler = document. ;
+bToggler. ;
+var clearB = ;
+clearB.;
+
